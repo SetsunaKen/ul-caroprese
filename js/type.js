60eea5da84af556311ddbf87fb62809cce44028b
Dario <dario@sestuna-ken.online>
2017-01-25 16:35:32 +0100

    first commit

---
+++ js/type.js
@@ -0,0 +1,20 @@
+$('#typeit').typeIt({
+     speed: 90,
+     autoStart: false
+	 lifeLike: true
+	})
+		.tiType('Hallo,')
+		.tiPause(800)
+		.tiType(' ich möchte eine Webseite.')
+		.tiPause(700)
+		.tiDelete(14)
+		.tiType('einen Blog starten.')
+		.tiPause(700)
+		.tiDelete(19)
+		.tiType('eCommerce betreiben.')
+		.tiPause(700)
+		.tiDelete(20)
+		.tiType('einen kompetenten Partner.')
+		.tiPause(700)
+		.tiDelete(26)
+		.tiType('eine professionelle Beratung.')
